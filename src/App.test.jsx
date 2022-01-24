58e0ade4bb8f5054df0fb85d27d05b5c8a6534b6
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-01-24 13:02:04 -0800

    written header tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import Header from './views/Layout/Layout'
+
+const user = {}
 
 test('Should render the header', () => {
+  render(<Header user={user} />)
 
+  const image = screen.getByAltText(/Alchemy Logo/i)
+  expect(image).toBeInTheDocument()
+
+  const username = screen.getByRole('banner', { paragraph: /meet/i })
+  expect(username).toBeInTheDocument()
 })
